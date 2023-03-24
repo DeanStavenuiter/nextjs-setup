0b55e5a6243f0d977670acbeff7b2e1b3be0b258
DeanStavenuiter <ddstavenuiter@gmail.com>
2023-03-24 19:33:18 +0100

    changed error messages

--- src/pages/api/auth/reset-password.ts
+++ src/pages/api/auth/reset-password.ts
@@ -1,44 +1,45 @@
 import type { NextApiRequest, NextApiResponse } from "next";
 import connectMongoDB from "../../../../db/connectMongoDB";
-import User from "../models/User"
+import User from "../models/User";
 import bcrypt from "bcrypt";
-
-const verifyResetToken = async (token: string) => {
-  const user = await User.findOne({
-    resetToken: token,
-    resetTokenExpiry: { $gt: Date.now() },
-  });
-
-  if (!user) {
-    throw new Error("Invalid or expired reset token");
-  }
-  return user;
-};
-
-const updatePassword = async (userId: string, password: string) => {
-  try {
-    const user = await User.findById(userId);
-    const salt = bcrypt.genSaltSync(12);
-
-    if (!user) {
-      throw new Error("User not found");
-    }
-
-    user.passwordHash = bcrypt.hashSync(password, salt);
-    user.resetToken = null;
-    user.resetTokenExpiry = null;
-
-    await user.save();
-  } catch (err) {
-    console.error(err);
-    throw new Error("Error updating password");
-  }
-};
 
 export const resetPasswordHandler = async (
   req: NextApiRequest,
   res: NextApiResponse
 ) => {
+  const verifyResetToken = async (token: string) => {
+    const user = await User.findOne({
+      resetToken: token,
+      resetTokenExpiry: { $gt: Date.now() },
+    });
+
+    if (!user) {
+      return res
+        .status(500)
+        .json({ message: "Invalid or expired reset token" });
+    }
+    return user;
+  };
+
+  const updatePassword = async (userId: string, password: string) => {
+    try {
+      const user = await User.findById(userId);
+      const salt = bcrypt.genSaltSync(12);
+
+      if (!user) {
+        return res.status(500).json({ message: "User not found" });
+      }
+
+      user.passwordHash = bcrypt.hashSync(password, salt);
+      user.resetToken = null;
+      user.resetTokenExpiry = null;
+
+      await user.save();
+    } catch (err) {
+      console.error(err);
+      throw new Error("Error updating password");
+    }
+  };
 
   if (req.method === "POST") {
     const { token, password } = req.body;
